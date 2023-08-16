b5f546b17dbdeb90290c6c6f11d86e2de8f1c4a4
ehtisham <ehtishama876@gmail.com>
2023-08-16 20:36:08 +0500

    initail comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5000');
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState('');
+  const [username, setUsername] = useState('');
+  const [userJoined, setUserJoined] = useState(false);
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    return () => {
+      // Clean up the event listener when the component unmounts
+      socket.off('message');
+    };
+  }, []);
+
+  const handleUsernameSubmit = (e) => {
+    e.preventDefault();
+  if (username.trim() !== '') {
+      setUsername(username.trim());
+      setUserJoined(true);
+      socket.emit('user-join', { username });
+    }
+  };
+
+  const handleMessageSubmit = (e) => {
+    e.preventDefault();
+    if (messageInput.trim() !== '') {
+      socket.emit('message', { text: messageInput, username });
+      setMessageInput('');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!userJoined ? (
+        <div className="username-prompt">
+        <form onSubmit={handleUsernameSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter your username"
+          />
+          <button type="submit">Join Chat</button>
+        </form>
+      </div>
+      ) : (
+        <div className="chat-container">
+          <div className="message-list">
+            {messages.map((message, index) => (
+              <div key={index} className="message">
+                {message.username}: {message.text}
+              </div>
+            ))}
+          </div>
+          <form className="message-form" onSubmit={handleMessageSubmit}>
+            <input
+              type="text"
+              value={messageInput}
+              onChange={(e) => setMessageInput(e.target.value)}
+              placeholder="Enter your message..."
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
